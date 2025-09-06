2fc3870b8004fbff0b61caf1dab27892603798c6
StarXD96 <iskitovakenje@gmail.com>
2025-09-06 19:05:59 +0600

    lesson_1

---
+++ main.py
@@ -0,0 +1,35 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'Моё первое приложение'
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    greeting_text = ft.Text(value='Hello World')
+
+    greeting_history = []
+    history_text = ft.Text('История приветствий:')
+
+
+    def on_button_click(_):
+        name = name_input.value.strip()
+        print(name)
+
+        if name:
+            greeting_text.value = f'Hello {name}!'
+            print(greeting_text)
+            name_input.value = ''
+
+            greeting_history.append(f'{name}')
+            history_text.value = 'История приветствий:\n' + '\n'.join(greeting_history)
+        else:
+            greeting_text.value = 'Пожалуйста, введите имя!'
+
+        page.update()
+
+    name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
+    name_button = ft.ElevatedButton(text='SEND', on_click=on_button_click)
+
+    page.add(greeting_text, name_input, name_button, history_text)
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)
