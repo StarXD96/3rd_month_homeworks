1d3745b400933a7129693b8194ce2589f9c82630
StarXD96 <iskitovakenje@gmail.com>
2025-09-10 13:28:09 +0600

    homework_4

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ def main(page: ft.Page):
 
     greeting_history = []
     history_text = ft.Text('История приветствий:')
-
+    history_visible = True
 
     def on_button_click(_):
         name = name_input.value.strip()
         print(name)
 
         if name:
-            
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
             greeting_text.value = f'Hello {name}! ({now})'
@@ -34,6 +33,7 @@ def main(page: ft.Page):
     def clear_history(_):
         greeting_history.clear()
         history_text.value = "История приветствий"
+        
         page.update()
 
     def theme_change(_):
@@ -41,14 +41,27 @@ def main(page: ft.Page):
             page.theme_mode = ft.ThemeMode.DARK
         else:
             page.theme_mode = ft.ThemeMode.LIGHT
+        
+        page.update()
+
+    def history_hide(_):
+        nonlocal history_visible
+        if history_visible:
+            history_visible = False
+            history_text.visible = False
+        else:
+            history_visible = True
+            history_text.visible = True
+
         page.update()
 
     name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
     name_button = ft.ElevatedButton(text='SEND', on_click=on_button_click)
     clear_history_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
     theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=theme_change, tooltip='Сменит тему')
-
-    page.add(greeting_text, name_input, name_button, clear_history_button, theme_button, history_text)
+    hide_history_button = ft.ElevatedButton(text='HIDE', on_click=history_hide, tooltip='Спрячет историю/покажет историю')
+    
+    page.add(greeting_text, name_input, name_button, clear_history_button, theme_button, hide_history_button, history_text)
 
 
 ft.app(target=main, view=ft.WEB_BROWSER)
